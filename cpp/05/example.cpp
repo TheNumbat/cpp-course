83c31bde799baa84f7c2474bbe3a179547b985b7
Maxwell Slater <mslater@nevada.unr.edu>
2016-07-15 15:18:30 -0700

    edit 5

--- cpp/05/example.cpp
+++ cpp/05/example.cpp
@@ -1,11 +1,11 @@
-//If you are not sure what some lines of code do, try looking back at
-//previous example programs, notes, or ask a question.
+// If you are not sure what some lines of code do, try looking back at
+// previous example programs, notes, or ask a question.
 
 #include <iostream>
 
 using namespace std;
 
-//Used for an if statement
+// Used for an if statement
 bool isNumGreat(int num);
 
 int main() {
@@ -17,8 +17,8 @@ int main() {
 	
 	cin >> number;
 	
-	//This chain of if/else staements discovers if your number causes the isNumGreat function to return true,
-	//or if your number is between 0 and 100, negative, or greather than 100
+	// This chain of if/else statements discovers if your number causes the isNumGreat function to return true,
+	// or if your number is between 0 and 100, negative, or greater than 100
 	if(isNumGreat(number)) {
 		
 		cout << "Your favorite number is the answer to life, the universe, and everything!" << endl;
@@ -41,9 +41,9 @@ int main() {
 	
 	cin >> character;
 	
-	//This switch statement detects if the letter is an a, or a b, c, or d, or an e - note that if you enter an
-	//e you will get the result from e and f, or an f. Finally, it can also default to a statement if you did
-	//not enter any of those letters.
+	// This switch statement detects if the letter is an 'a;' or a 'b,' 'c,' or 'd;' or an 'e;' or an 'f.' If you enter an
+	// 'e' you will get the result from 'e' and 'f.' Finally, it can also default to a statement if you did
+	// not enter any of those letters.
 	switch(character) {
 		case 'a':
 			cout << "The next letter is 'b'" << endl;
@@ -68,6 +68,6 @@ int main() {
 }
 
 bool isNumGreat(int num) {
-	//Num == 42 is a boolean statement, so the function can return the result of it.
+	// num == 42 is a boolean statement, so the function can return the result of it.
 	return num == 42;
 }
