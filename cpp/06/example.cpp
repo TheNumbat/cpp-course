6b4fd264e163683b9514142989d9221438d08f46
Maxwell Slater <mslater@nevada.unr.edu>
2016-07-18 13:45:39 -0700

    edit 6

--- cpp/06/example.cpp
+++ cpp/06/example.cpp
@@ -1,5 +1,5 @@
-//If you are not sure what some lines of code do, try looking back at
-//previous example programs, notes, or ask a question.
+// If you are not sure what some lines of code do, try looking back at
+// previous example programs, notes, or ask a question.
 
 #include <iostream>
 
@@ -11,8 +11,8 @@ int main() {
 	
 	char selection;
 	
-	//A simple do/while loop, where the loop will continue asking the user
-	//to continue until they enter something that is not 'y'
+	// A simple do/while loop, where the loop will continue asking the user
+	// to continue until they enter something that is not 'y'
 	do {
 		cout << "Continue? (y/n): ";
 		cin >> selection;
@@ -20,10 +20,10 @@ int main() {
 	
 	cout << endl << endl;
 	
-	//A nested for loop, will run the inner loop 10 times. Each time the outer
-	//loop is run, i is incremented, and hence the first output number goes up
-	//every ten outputs. Within each run of the outer loop, the inner loop sets
-	//j to 0, and counts it up to 10, which produces each pair of outpout numbers.
+	// A nested for loop, will run the inner loop 10 times. Each time the outer
+	// loop is run, i is incremented, and hence the first output number goes up
+	// every ten outputs. Within each run of the outer loop, the inner loop sets
+	// j to 0, and counts it up to 10, which produces each pair of output numbers.
 	for(int i = 0; i < 10; i++) {
 		for(int j = 0; j < 10; j++) {
 			cout << i << "-" << j << " ";
@@ -33,7 +33,7 @@ int main() {
 	
 	cout << endl << endl;
 	
-	//This calls the recursive function
+	// This calls the recursive function
 	cout << "Recursion example: " << addFrom(5) << endl << endl;
 	
 	system("pause");
@@ -41,7 +41,7 @@ int main() {
 	return 0;
 }
 
-//This is a recursive function. See the notes for how it works.
+// This is a recursive function. See the notes for how it works.
 int addFrom(int x) {
 	
 	if(x == 1)
