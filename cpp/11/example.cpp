04f9542960785c633495034e8da6a0c8f2b74835
Maxwell Slater <mslater@nevada.unr.edu>
2016-08-13 21:46:32 -0700

    edit 11

--- cpp/11/example.cpp
+++ cpp/11/example.cpp
@@ -1,5 +1,5 @@
-//If you are not sure what some lines of code do, try looking back at
-//previous example programs, notes, or ask a question.
+// If you are not sure what some lines of code do, try looking back at
+// previous example programs, notes, or ask a question.
 
 #include <iostream>
 
@@ -11,57 +11,57 @@ char* getNewStr(int size);
 
 int main() {
 
-	//Declaring an array on the stack vs. on the heap
+	// Declaring an array on the stack vs. on the heap
 	char stackStr[50];
 	char* heapStr = new char[50];
 
-	//Both arrays can be used in the same ways
+	// Both arrays can be used in the same ways
 	for(int i = 0; i < 50; i++) {
 		stackStr[i] = 'a';
 		heapStr[i] = 'a';
 	}
 
-	//However, the string on the heap must be deleted when
-	//you are done with it--if you never deleted it is considered
-	//leaked memory.
+	// However, the string on the heap must be deleted when
+	// you are done with it--if you never deleted it is considered
+	// leaked memory.
 	delete[] heapStr;
 	heapStr = NULL;
 
-	//Here, you can see how important is to delete your memory
-	//before you lose access to it, or else this loop would
-	//leak a ton of memoroy.
+	// Here, you can see how important is to delete your memory
+	// before you lose access to it, or else this loop would
+	// leak a ton of memory.
 	for(int i = 0; i < 50; i++) {
 		heapStr = new char[100];
 
-		//If the memory was not deleted, a new set of memory
-		//would be assigned to "heapStr," and the previous
-		//memory would be leaked.
+		// If the memory was not deleted, a new set of memory
+		// would be assigned to "heapStr," and the previous
+		// memory would be leaked.
 		delete[] heapStr;
 		heapStr = NULL;
 	}
 
-	//Input a buffered string
+	// Input a buffered string
 	char buffer[100];
 	cout << endl << "Enter a string: ";
 	cin >> buffer;
 
-	//Allocate the exact number of characters neccesary.
+	// Allocate the exact number of characters necessary.
 	heapStr = new char[strlen(buffer) + 1];
 
-	//Copy the string
+	// Copy the string
 	strcpy(heapStr,buffer);
 
 	cout << endl << "Exactly sized word: " << heapStr << endl << endl;
 
-	//Delete the string
+	// Delete the string
 	delete[] heapStr;
 	heapStr = NULL;
 
-	//Finally, here is a function that will return dynamic meory.
+	// Finally, here is a function that will return dynamic memory.
 	char* newStr = getNewStr(10);
 
-	//the memory origionally created in the funciton can be deleted
-	//here, at the end of main
+	// the memory originally created in the function can be deleted
+	// here, at the end of main
 	delete[] newStr;
 
 
@@ -71,17 +71,17 @@ int main() {
 
 char* getNewStr(int size) {
 
-	//Create a new dynamically allocated character array and return it.
-	//The calling funciton will still have access to the memory, so it
-	//does not need to be deleted here.
+	// Create a new dynamically allocated character array and return it.
+	// The calling function will still have access to the memory, so it
+	// does not need to be deleted here.
 	return new char[size];
 }
 
 int strlen(const char* str) {
 	int counter = 0;
 
-	//Loop until you find a value that equals 0, the null char
-	//(which marks the end of the string)
+	// Loop until you find a value that equals 0, the null char
+	// (which marks the end of the string)
 	while(str[counter])
 		counter++;
 
@@ -92,12 +92,12 @@ int strcpy(char* dest, const char* src) {
 
 	int counter;
 
-	//Loop along the length of the source string,
-	//and copy each character
+	// Loop along the length of the source string,
+	// and copy each character
 	for(counter = 0; counter < strlen(src); counter++)
 		dest[counter] = src[counter];
 
-	//Remember to add a null character at the end
-	//of the destination string
+	// Remember to add a null character at the end
+	// of the destination string
 	dest[counter] = '\0';
 }
